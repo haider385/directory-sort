bebcb0cd9aea49fcf39df06e3729525e6ded55ec
Haider <haider.ali385@gmail.com>
2021-09-13 21:11:00 +0100

    added image file types

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 SUBDIRECTORIES = {
     "DOCUMENTS": ['.pdf', '.rtf', '.txt', '.csv'],
     "AUDIO": ['.m4a', '.m4b', '.mp3'],
-    "VIDEOS": ['.mov', '.avi', '.mp4']}
+    "VIDEOS": ['.mov', '.avi', '.mp4'],
+    "IMAGES": ['.jpg', '.jpeg', '.png']
+    }

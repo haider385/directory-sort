ed5647b1a88451f2fad7af62e53b211d670d309f
Haider <haider.ali385@gmail.com>
2021-09-16 22:46:46 +0100

    created 'chooseDirectory'

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ SUBDIRECTORIES = {
     "VIDEOS": ['.mov', '.avi', '.mp4'],
     "IMAGES": ['.jpg', '.jpeg', '.png']
     }
+
+def chooseDirectory(value):
+    for category, extensions in SUBDIRECTORIES.items():
+        for extension in extensions:
+            if extension == value:
+                return category
